0ce3e57d0acf21a8bc7595c44bb371cddcb5092f
jlrconst <jlconsttantino@live.com>
2024-06-26 15:10:03 -0300

    better results page; now all expected answers are displayed; added informations about correctitude of the model answers; now the scores are calculated by getting the max score along all possible answers

--- models/model_output_loader.py
+++ models/model_output_loader.py
@@ -1,7 +1,7 @@
 # General dependencies
 import pandas as pd
 
-def load_outputs(csv_path: str) -> tuple[dict[tuple[int,int,int], str], dict[tuple[int,int,int], str]]:
+def load_outputs(csv_path: str) -> tuple[dict[tuple[int,int,int], tuple[str, bool]], dict[tuple[int,int,int], tuple[str, bool]]]:
     '''
     Loads the outputs from CSV file as a dictionary.
 
@@ -14,12 +14,13 @@ def load_outputs(csv_path: str) -> tuple[dict[tuple[int,int,int], str], dict[tup
     Returns:
     -------
 
-    symbolic_answers: dict[tuple[int,int,int], str]
+    symbolic_answers: dict[tuple[int,int,int], tuple[str, bool]]
         The answers for the symbolic model. Each key is a tuple containing 
         the indexes, in this order, for the topic, the context and the 
-        question. Each value is the textual answer of the model.
+        question. Each value is the textual answer of the model, followed 
+        by a boolean indicating if this answer is correct.
 
-    neural_answers: dict[tuple[int,int,int], str]
+    neural_answers: dict[tuple[int,int,int], tuple[str, bool]]
         Same as the first output, but for the neural model. 
     '''
     # Loads the data
@@ -30,8 +31,12 @@ def load_outputs(csv_path: str) -> tuple[dict[tuple[int,int,int], str], dict[tup
     neural_answers = {}
 
     # Fetchs the data
-    for _, (topic_idx, context_idx, question_idx, symbolic_answer, neural_answer) in df_outputs.iterrows():
-        symbolic_answers[(topic_idx, context_idx, question_idx)] = symbolic_answer
-        neural_answers[(topic_idx, context_idx, question_idx)] = neural_answer
+    for _, (
+        topic_idx, context_idx, question_idx, 
+        symbolic_answer, neural_answer, 
+        symbolic_is_correct, neural_is_correct
+    ) in df_outputs.iterrows():
+        symbolic_answers[(topic_idx, context_idx, question_idx)] = (symbolic_answer, symbolic_is_correct)
+        neural_answers[(topic_idx, context_idx, question_idx)] = (neural_answer, neural_is_correct)
 
     return symbolic_answers, neural_answers
--- source/result_page.py
+++ source/result_page.py
@@ -1,10 +1,11 @@
 # General dependencies
 import numpy as np
+import pandas as pd
 import streamlit as st
 
 # Local dependencies
 from source.available_pages import Pages
-from source.answer_checker import check_answer_from_text
+from source.faquad import FaquadDataset
 from models.model_output_loader import load_outputs
 from models.metrics import compute_f1, exact_match
 
@@ -23,8 +24,6 @@ def _clear_game():
         "user_answered_questions_indexes", 
         "generated_results", 
         "scores_results", 
-        "final_questions", 
-        "final_answers"
     ]
     for state in states_to_clear:
         if state in st.session_state:
@@ -39,6 +38,19 @@ def _generate_status_message(hits, question_idx):
         st.error("Incorreto")
 
 
+def _preprocess_user_answer(user_selections) -> str:
+    answer = ""
+    for selection in sorted(user_selections, key=lambda x: x["start"]):
+        answer += selection["text"]
+    return answer
+
+
+def _get_ground_truth(dataset, title, context_idx, question_idx) -> list[str]:
+    ground_truth = dataset.get_answers(title, context_idx, question_idx)
+    ground_truth = [answer["text"] for answer in ground_truth]
+    return ground_truth
+
+
 def generate_results_page():
     '''
     Generates the page of the user results.
@@ -49,6 +61,10 @@ def generate_results_page():
 
     dataset: FaquadDataset
         The dataset for the QA Game.
+
+    user_correct_answers: dict[tuple[int,int,int], bool]
+        Indicates, for a tuple of indexes of a topic, a paragraph and 
+        a question, if the user correctly answered the question.
     
     user_textual_answers: dict[tuple[int,int,int], list[str]]
         Indicates, for a tuple of indexes of a topic, a paragraph and 
@@ -69,25 +85,15 @@ def generate_results_page():
         underline ("_") and the name of the agent ("user", 
         "symbolic" and "neural"). Except "hit": this contains 
         boolean masks as values.
-    
-    final_questions: list[str]
-        List of the processed questions.
-    
-    final_answers: dict[str, list[str]]
-        Answers of the agents for every one of the final questions. 
-        The names of the agents are "user", "symbolic", "neural" 
-        and "expected".
     '''
+    # Loads the dataset and the answers
+    dataset: FaquadDataset = st.session_state["dataset"]
+    user_correct_answers = st.session_state["user_correct_answers"]
+    user_textual_answers = st.session_state["user_textual_answers"]
+    symbolic_answers_dict, neural_answers_dict = load_outputs("./data/models_answers.csv")
+
     # Process the results if not processed yet
     if "generated_results" not in st.session_state:
-
-         # Gets the dataset and the answers
-        dataset = st.session_state["dataset"]
-        user_textual_answers = st.session_state["user_textual_answers"]
-        symbolic_answers_dict, neural_answers_dict = load_outputs("./data/models_answers.csv")
-
-        # To store the questions
-        questions = []
 
         # To hold the scores
         user_f1_scores = []
@@ -100,60 +106,60 @@ def generate_results_page():
         neural_em_scores = []
         neural_hit_scores = []
 
-        # To hold the answers of the models
-        expected_answers = []
-        user_answers = []
-        symbolic_answers = []
-        neural_answers = []
-
         # Computes scores
         for (title_idx, context_idx, question_idx), user_selections in user_textual_answers.items():
 
-            # Pre-process the answer
-            answer = ""
-            for selection in sorted(user_selections, key=lambda x: x["start"]):
-                answer  += selection["text"]
+            # Pre-process the answer of the user
+            user_answer = _preprocess_user_answer(user_selections)
+            user_is_correct = user_correct_answers[title_idx, context_idx, question_idx]
+
+            # Answer of the symbolic model
+            symbolic_answer, symbolic_is_correct = symbolic_answers_dict[title_idx, context_idx, question_idx]
+
+            # Answer of the neural model
+            neural_answer, neural_is_correct = neural_answers_dict[title_idx, context_idx, question_idx]
 
             # Title (topic)
             title = dataset.sorted_titles[title_idx]
 
-            # Gets first answer as the ground-truth
-            ground_truth = dataset.get_answers(title, context_idx, question_idx)[0]["text"]
-            expected_answers.append(ground_truth)
+            # Gets answers as the ground-truth
+            ground_truth = _get_ground_truth(dataset, title, context_idx, question_idx)
 
-            # Computes scores for the user
-            user_answers.append(answer)
-            user_f1_scores.append(compute_f1(answer, ground_truth))
-            user_em_scores.append(exact_match(answer, ground_truth))
-            user_hit_scores.append(check_answer_from_text(answer, ground_truth))
+            # Temporary score holders in order to get the max score among the different expected answers
+            temp_user_f1_scores = []
+            temp_user_em_scores = []
+            temp_symbolic_f1_scores = []
+            temp_symbolic_em_scores = []
+            temp_neural_f1_scores = []
+            temp_neural_em_scores = []
 
-            # Gets both context and question
-            context = dataset.get_context(title, context_idx)
-            question = dataset.get_question(title, context_idx, question_idx)
-            questions.append(question)
+            # Computes the different scores for different expected answers
+            for expected_answer in ground_truth:
 
-            # Computes scores for the sybolic model
-            symbolic_answer = symbolic_answers_dict[title_idx, context_idx, question_idx]
-            symbolic_answers.append(symbolic_answer)
-            symbolic_f1_scores.append(compute_f1(symbolic_answer, ground_truth))
-            symbolic_em_scores.append(exact_match(symbolic_answer, ground_truth))
-            symbolic_hit_scores.append(check_answer_from_text(symbolic_answer, ground_truth))
+                # Computes scores for the user
+                temp_user_f1_scores.append(compute_f1(user_answer, expected_answer))
+                temp_user_em_scores.append(exact_match(user_answer, expected_answer))
 
-            # Computes scores for the neural model
-            neural_answer = neural_answers_dict[title_idx, context_idx, question_idx]
-            neural_answers.append(neural_answer)
-            neural_f1_scores.append(compute_f1(neural_answer, ground_truth))
-            neural_em_scores.append(exact_match(neural_answer, ground_truth))
-            neural_hit_scores.append(check_answer_from_text(neural_answer, ground_truth))
+                # Computes scores for the sybolic model
+                temp_symbolic_f1_scores.append(compute_f1(symbolic_answer, expected_answer))
+                temp_symbolic_em_scores.append(exact_match(symbolic_answer, expected_answer))
+
+                # Computes scores for the neural model
+                temp_neural_f1_scores.append(compute_f1(neural_answer, expected_answer))
+                temp_neural_em_scores.append(exact_match(neural_answer, expected_answer))
+            
+            # Saves the max scores
+            user_f1_scores.append(max(temp_user_f1_scores))
+            user_em_scores.append(max(temp_user_em_scores))
+            user_hit_scores.append(user_is_correct)
+            symbolic_f1_scores.append(max(temp_symbolic_f1_scores))
+            symbolic_em_scores.append(max(temp_symbolic_em_scores))
+            symbolic_hit_scores.append(symbolic_is_correct)
+            neural_f1_scores.append(max(temp_neural_f1_scores))
+            neural_em_scores.append(max(temp_neural_em_scores))
+            neural_hit_scores.append(neural_is_correct)
 
         # Loading everything to session state
-        st.session_state["final_questions"] = questions
-        st.session_state["final_answers"] = {
-            "user": user_answers, 
-            "symbolic": symbolic_answers, 
-            "neural": neural_answers, 
-            "expected": expected_answers
-        }
         st.session_state["scores_results"] = {
             "f1_user": (np.mean(user_f1_scores), np.std(user_f1_scores)), 
             "f1_symbolic": (np.mean(symbolic_f1_scores), np.std(symbolic_f1_scores)), 
@@ -168,13 +174,6 @@ def generate_results_page():
         st.session_state["generated_results"] = True
 
         # Freeing memory
-        del dataset
-        del user_textual_answers
-        del questions
-        del expected_answers
-        del user_answers
-        del symbolic_answers
-        del neural_answers
         del user_f1_scores
         del user_em_scores
         del user_hit_scores
@@ -184,22 +183,31 @@ def generate_results_page():
         del neural_f1_scores
         del neural_em_scores
         del neural_hit_scores
-        del symbolic_answers_dict
-        del neural_answers_dict
 
     # Loads everything necessary from session state
-    questions = st.session_state["final_questions"]
-    answers = st.session_state["final_answers"]
     scores = st.session_state["scores_results"]
     
     # Page structure (first half)
     st.title("**Resultados**")
     st.divider()
+
+    # Results data
+    df_results = pd.DataFrame(np.array([
+            [np.sum(scores["hit_symbolic"]), "{:.2f} ± {:.2f}".format(*scores["f1_symbolic"]), "{:.2f} ± {:.2f}".format(*scores["em_symbolic"])], 
+            [np.sum(scores["hit_user"]), "{:.2f} ± {:.2f}".format(*scores["f1_user"]), "{:.2f} ± {:.2f}".format(*scores["em_user"])], 
+            [np.sum(scores["hit_neural"]), "{:.2f} ± {:.2f}".format(*scores["f1_neural"]), "{:.2f} ± {:.2f}".format(*scores["em_neural"])], 
+        ]).T, 
+        columns=["🐌 O Caracol", "😄 Usuário (Você)", "👑 Bert"],
+        index=["Total de Acertos", "Pontuação F1", "Casamento Exato"])
+    
+    # Writes the results
+    st.dataframe(df_results, use_container_width=True)
+    """
     cols = st.columns(3)
 
     # Symbolic results
     with cols[0]:
-        st.markdown("## 🐌 **Modelo Simbólico**")
+        st.markdown("## 🐌 **O Caracol**")
         st.markdown('''
             > **Pontuação F1:**
             >> {:.2f} ± {:.2f}
@@ -225,7 +233,7 @@ def generate_results_page():
 
     # Neural results
     with cols[2]:
-        st.markdown("## 🦅 **Modelo Neural (Bert)**")
+        st.markdown("## 👑 **Bert**")
         st.markdown('''
             > **Pontuação F1:**
             >> {:.2f} ± {:.2f}
@@ -235,41 +243,55 @@ def generate_results_page():
 
             > **Total de acertos:**
             >> {}'''.format(*scores["f1_neural"], *scores["em_neural"], np.sum(scores["hit_neural"])))
+    """
 
     # Page structure (second half)
     st.divider()
     st.markdown("## Comparar respostas")
 
+    # Gets the questions
+    questions = [
+        (
+            tidx, cidx, qidx, 
+            dataset.get_question(dataset.sorted_titles[tidx], cidx, qidx) 
+        )
+        for tidx, cidx, qidx in user_textual_answers.keys()]
+
     # Question selection
     question_idx = st.selectbox(
         "Selecione uma questão:", 
         range(len(questions)), 
-        format_func=lambda x: questions[x])
+        format_func=lambda x: questions[x][-1])
+    
+    # Gets the answers
+    tidx, cidx, qidx, _ = questions[question_idx]
+    title = dataset.sorted_titles[tidx]
+    expected_answers = [answer["text"] for answer in dataset.get_answers(title, cidx, qidx)]
+    user_answer = _preprocess_user_answer(user_textual_answers[tidx, cidx, qidx])
+    symbolic_answer, _ = symbolic_answers_dict[tidx, cidx, qidx]
+    neural_answer, _ = neural_answers_dict[tidx, cidx, qidx]
     
     # Expected answer display
-    st.selectbox("Resposta esperada:", [answers["expected"][question_idx]])
+    st.selectbox("Respostas possíveis esperadas:", expected_answers)
 
     # Symbolic answer
     cols = st.columns(3)
     with cols[0]:
-        st.markdown("## 🐌 **Modelo Simbólico**")
+        st.markdown("## 🐌 **O Caracol**")
         _generate_status_message(scores["hit_symbolic"], question_idx)
-        answer = answers["symbolic"][question_idx]
-        st.write(answer if len(answer) > 0 else "...")
+        st.write(symbolic_answer if len(symbolic_answer) > 0 else "...")
     
     # User answer
     with cols[1]:
         st.markdown("## 😄 **Usuário (Você)**")
         _generate_status_message(scores["hit_user"], question_idx)
-        answer = answers["user"][question_idx]
-        st.write(answer if len(answer) > 0 else "...")
+        st.write(user_answer if len(user_answer) > 0 else "...")
 
     # Neural answer
     with cols[2]:
-        st.markdown("## 🦅 **Modelo Neural (Bert)**")
+        st.markdown("## 👑 **Bert**")
         _generate_status_message(scores["hit_neural"], question_idx)
-        answer = answers["neural"][question_idx]
-        st.write(answer if len(answer) > 0 else "...")
+        st.write(neural_answer if len(neural_answer) > 0 else "...")
 
     # Clear game button
     st.divider()
